c282480a74acf535e99c0898a2ff81e0ea01e00a
DonEvans <don9450-register@yahoo.com>
2022-04-02 18:03:05 -0700

    Modified topics routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  
+
   get 'messages/create'
   get 'messages/index'
   root 'messages#index'
-  
+
   get '/signin', to: 'sessions#new'
   post 'signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
-  #get 'topics/new'
+  get '/topics/new'
   #get 'topics/create'
   #get 'topics/show'
   #get 'topics/index'
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   resources :users
   resources :messages
   resources :topics
-  
+
 end
--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class TopicsControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
-    get new_topic_url
+    get new_topic_path
     assert_response :success
   end
-  
+
 end
