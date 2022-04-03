d043a7ac7b623ad206fa66370d4a4f545182661e
DonEvans <don9450-register@yahoo.com>
2022-04-02 18:21:51 -0700

    Commit to resolve heroku problem

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class TopicsControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
-    get new_topic_path
+    get new_topic_url
     assert_response :success
   end
 
